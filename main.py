4f41bffb69a00200ccec848a03615f9915c90729
Sean <seaneredmond@gmail.com>
2022-10-29 23:04:09 +0100

    another commit..test

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 def print_hi():
     print("test uploading project to GitHub")
     print("another comment to practice committing project")
-    print("another comment")#test
-
+    print("another comment")  # test
+    print("yet another")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi()
-
-
